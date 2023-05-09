dcf35ab51420d483de24ac1a9c54123d4ae6c347
Max_Nikitenok <xxx250790@gmail.com>
2023-05-09 13:31:55 +0300

    feat: main page

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import styles from './page.module.css';
+import { SlSocialInstagram } from 'react-icons/sl';
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+          <div className={styles.instaLink}>
+            <SlSocialInstagram size={24} />
+          </div>
+          <div className={styles.leftLinks}>
+            <div>Главная</div>
+            <div>Мастера</div>
+            <div>Косметика</div>
+          </div>
+          <div className={styles.logo}>
+            <Image src="/logo.png" width={40} height={38} alt="logo" />
+          </div>
+          <div className={styles.rightLinks}>
+            <div>Акции</div>
+            <div>Отзывы</div>
+            <div>Контакты</div>
+          </div>
+          <div className={styles.menu}>|||</div>
+        </nav>
+  );
+};
+
+export default Navbar;
