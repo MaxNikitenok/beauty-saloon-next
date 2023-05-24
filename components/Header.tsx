48276b87656de423fbf2c082d6d5eb7082c93648
Max_Nikitenok <xxx250790@gmail.com>
2023-05-24 22:35:45 +0300

    fix: header scroll

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,154 +13,159 @@ const Header = () => {
 
   const [showMenu, setShowMenu] = useState(false);
 
-  const [scroll, setScroll] = useState(0);
-
-  console.log(scroll)
+  const [scroll, setScroll] = useState(window.scrollY);
 
   const handleScroll = () => {
     setScroll(window.scrollY);
   };
 
   useEffect(() => {
+    window.scrollTo({
+      top: 0,
+  });
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
     <header className={scroll > 60 ? styles.minHeader : styles.header}>
-      <div className={styles.wrapper}>
-        <nav className={scroll > 60 ? styles.minNavbar : styles.navbar}>
-          <a href="https://www.instagram.com/" className={styles.instaLink}>
-            <SlSocialInstagram size={24} />
-          </a>
-          <div className={styles.leftLinks}>
-            <Link
-              href="/"
-              className={pathname === "/" ? styles.link_active : styles.link}
-            >
-              Главная
-            </Link>
-            <Link
-              href="/our-team"
-              className={
-                pathname === "/our-team" ? styles.link_active : styles.link
-              }
-            >
-              Мастера
-            </Link>
-            <Link
-              href="/cosmetics"
-              className={
-                pathname === "/cosmetics" ? styles.link_active : styles.link
-              }
-            >
-              Косметика
-            </Link>
-          </div>
-          <Link href="/" className={styles.logo}>
-            <Image src="/logo.png" width={40} height={38} alt="logo" />
-          </Link>
-          <div className={styles.rightLinks}>
-            <Link
-              href="/price"
-              className={
-                pathname === "/price" ? styles.link_active : styles.link
-              }
-            >
-              Цены
-            </Link>
-            <Link
-              href="/reviews"
-              className={
-                pathname === "/reviews" ? styles.link_active : styles.link
-              }
-            >
-              Отзывы
-            </Link>
-            <Link
-              href="/contacts"
-              className={
-                pathname === "/contacts" ? styles.link_active : styles.link
-              }
-            >
-              Контакты
-            </Link>
-          </div>
-          <div className={styles.menuButton_container}>
-            <div
-              className={styles.menuButton}
-              onClick={() => setShowMenu(true)}
-            >
-              <div
+      <div className={scroll > 60 ? styles.headerBackMin : styles.headerBackFull}>
+        <div className={styles.wrapper}>
+          <nav className={styles.navbar}>
+            <a href="https://www.instagram.com/" className={styles.instaLink}>
+              <SlSocialInstagram size={24} />
+            </a>
+            <div className={styles.leftLinks}>
+              <Link
+                href="/"
+                onClick={() => setScroll(window.scrollY)}
+                className={pathname === "/" ? styles.link_active : styles.link}
+              >
+                Главная
+              </Link>
+              <Link
+                href="/our-team"
+                onClick={() => setScroll(window.scrollY)}
                 className={
-                  showMenu
-                    ? styles.topLine_position2
-                    : styles.menuButton_topLine
+                  pathname === "/our-team" ? styles.link_active : styles.link
                 }
-              ></div>
-              <div
+              >
+                Мастера
+              </Link>
+              <Link
+                href="/cosmetics"
                 className={
-                  showMenu
-                    ? styles.centerLine_position2
-                    : styles.menuButton_centerLine
+                  pathname === "/cosmetics" ? styles.link_active : styles.link
                 }
-              ></div>
-              <div
-                className={
-                  showMenu
-                    ? styles.bottomLine_position2
-                    : styles.menuButton_bottomLine
-                }
-              ></div>
+              >
+                Косметика
+              </Link>
             </div>
-          </div>
-        </nav>
-        {showMenu && (
-          <div
-            className={styles.popupWrapper}
-            onClick={(e) => {
-              e.stopPropagation();
-              setShowMenu(false);
-            }}
-          ></div>
-        )}
-        <div
-          className={showMenu ? styles.showPopup : styles.hidePopup}
-          onClick={(e) => e.stopPropagation()}
-        >
-          <div className={styles.popup}>
-            <div
-              className={styles.popup_cross}
-              onClick={() => setShowMenu(false)}
-            >
-              <RxCross1 size={28} color="#fff" />
+            <Link href="/" className={styles.logo}>
+              <Image src="/logo.png" width={40} height={38} alt="logo" />
+            </Link>
+            <div className={styles.rightLinks}>
+              <Link
+                href="/price"
+                className={
+                  pathname === "/price" ? styles.link_active : styles.link
+                }
+              >
+                Цены
+              </Link>
+              <Link
+                href="/reviews"
+                className={
+                  pathname === "/reviews" ? styles.link_active : styles.link
+                }
+              >
+                Отзывы
+              </Link>
+              <Link
+                href="/contacts"
+                className={
+                  pathname === "/contacts" ? styles.link_active : styles.link
+                }
+              >
+                Контакты
+              </Link>
             </div>
-            <div className={styles.popup_content}>
-              <p className={styles.popup_text}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-                corrupti autem atque quidem voluptatum, itaque sit perferendis a
-                officiis doloremque mollitia consequatur consectetur tenetur ea
-                labore rerum porro soluta recusandae!
-              </p>
-              <div className={styles.popup_block}>
-                <div className={styles.block_title}>Контакты</div>
-                <div className={styles.block_description}>
-                  +7 (812) 123-45-67
-                </div>
-                <div className={styles.block_description}>
-                  +7 (812) 123-45-67
-                </div>
-                <div className={styles.block_description}>
-                  Новоостровский проспект, дом 36 лит. С
-                </div>
+            <div className={styles.menuButton_container}>
+              <div
+                className={styles.menuButton}
+                onClick={() => setShowMenu(true)}
+              >
+                <div
+                  className={
+                    showMenu
+                      ? styles.topLine_position2
+                      : styles.menuButton_topLine
+                  }
+                ></div>
+                <div
+                  className={
+                    showMenu
+                      ? styles.centerLine_position2
+                      : styles.menuButton_centerLine
+                  }
+                ></div>
+                <div
+                  className={
+                    showMenu
+                      ? styles.bottomLine_position2
+                      : styles.menuButton_bottomLine
+                  }
+                ></div>
               </div>
-              <div className={styles.popup_block}>
-                <div className={styles.block_title}>Режим работы</div>
-                <div className={styles.block_description}>
-                  C 10:00 до 21:00 (Пн-Пт)
+            </div>
+          </nav>
+          {showMenu && (
+            <div
+              className={styles.popupWrapper}
+              onClick={(e) => {
+                e.stopPropagation();
+                setShowMenu(false);
+              }}
+            ></div>
+          )}
+          <div
+            className={showMenu ? styles.showPopup : styles.hidePopup}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className={styles.popup}>
+              <div
+                className={styles.popup_cross}
+                onClick={() => setShowMenu(false)}
+              >
+                <RxCross1 size={28} color="#fff" />
+              </div>
+              <div className={styles.popup_content}>
+                <p className={styles.popup_text}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Dolores corrupti autem atque quidem voluptatum, itaque sit
+                  perferendis a officiis doloremque mollitia consequatur
+                  consectetur tenetur ea labore rerum porro soluta recusandae!
+                </p>
+                <div className={styles.popup_block}>
+                  <div className={styles.block_title}>Контакты</div>
+                  <div className={styles.block_description}>
+                    +7 (812) 123-45-67
+                  </div>
+                  <div className={styles.block_description}>
+                    +7 (812) 123-45-67
+                  </div>
+                  <div className={styles.block_description}>
+                    Новоостровский проспект, дом 36 лит. С
+                  </div>
                 </div>
-                <div className={styles.block_description}>
-                  С 11:00 до 20:00 (Сб-Вс)
+                <div className={styles.popup_block}>
+                  <div className={styles.block_title}>Режим работы</div>
+                  <div className={styles.block_description}>
+                    C 10:00 до 21:00 (Пн-Пт)
+                  </div>
+                  <div className={styles.block_description}>
+                    С 11:00 до 20:00 (Сб-Вс)
+                  </div>
                 </div>
               </div>
             </div>
