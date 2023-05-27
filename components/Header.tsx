4d0687b703da1dab39e2cab6ce6e2fde1176a96d
Max_Nikitenok <xxx250790@gmail.com>
2023-05-28 01:26:03 +0300

    feat: finish adaptive

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
 
   const [showMenu, setShowMenu] = useState(false);
 
-  let positionScrollY
+  let positionScrollY;
 
   if (typeof window !== "undefined") {
     positionScrollY = window.scrollY;
@@ -178,52 +178,54 @@ const Header = () => {
                 </div>
               </div>
               <div className={styles.popup_nav}>
-              <Link
-                href="/"
-                className={pathname === "/" ? styles.link_active : styles.link}
-              >
-                Главная
-              </Link>
-              <Link
-                href="/our-team"
-                className={
-                  pathname === "/our-team" ? styles.link_active : styles.link
-                }
-              >
-                Мастера
-              </Link>
-              <Link
-                href="/cosmetics"
-                className={
-                  pathname === "/cosmetics" ? styles.link_active : styles.link
-                }
-              >
-                Косметика
-              </Link>
-              <Link
-                href="/price"
-                className={
-                  pathname === "/price" ? styles.link_active : styles.link
-                }
-              >
-                Цены
-              </Link>
-              <Link
-                href="/reviews"
-                className={
-                  pathname === "/reviews" ? styles.link_active : styles.link
-                }
-              >
-                Отзывы
-              </Link>
-              <Link
-                href="/contacts"
-                className={
-                  pathname === "/contacts" ? styles.link_active : styles.link
-                }
-              >
-                Контакты
-              </Link>
+                <Link
+                  href="/"
+                  className={
+                    pathname === "/" ? styles.link_active : styles.link
+                  }
+                >
+                  Главная
+                </Link>
+                <Link
+                  href="/our-team"
+                  className={
+                    pathname === "/our-team" ? styles.link_active : styles.link
+                  }
+                >
+                  Мастера
+                </Link>
+                <Link
+                  href="/cosmetics"
+                  className={
+                    pathname === "/cosmetics" ? styles.link_active : styles.link
+                  }
+                >
+                  Косметика
+                </Link>
+                <Link
+                  href="/price"
+                  className={
+                    pathname === "/price" ? styles.link_active : styles.link
+                  }
+                >
+                  Цены
+                </Link>
+                <Link
+                  href="/reviews"
+                  className={
+                    pathname === "/reviews" ? styles.link_active : styles.link
+                  }
+                >
+                  Отзывы
+                </Link>
+                <Link
+                  href="/contacts"
+                  className={
+                    pathname === "/contacts" ? styles.link_active : styles.link
+                  }
+                >
+                  Контакты
+                </Link>
               </div>
             </div>
           </div>
